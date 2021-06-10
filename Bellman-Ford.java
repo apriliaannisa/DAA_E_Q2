c7b197feaced2dd862989646f62e6e28a9b8de69
Nurul Izzatil Ulum <76694068+nurululum15@users.noreply.github.com>
2021-06-10 16:53:14 +0700

    Update Bellman-Ford.java

--- Bellman-Ford.java
+++ Bellman-Ford.java
@@ -29,4 +29,38 @@ public class BellmanFord {
             return " Jarak antara " + this.value + " dari source, bernilai " + this.distance + " dan jalan yang di lalui adalah " + this.path;
         }
     }
+
+    public static void main(String[] args) {
+        Scanner masuk = new Scanner(System.in);
+        int nodes = masuk.nextInt();
+        int[][] graph = new int[nodes + 1][nodes + 1];
+        int numEdges = masuk.nextInt();
+        for (int edge = 0; edge < numEdges; edge++) {
+            int x = masuk.nextInt(), y = masuk.nextInt(), w = masuk.nextInt();
+            graph[x][y] = w;
+        }
+        bellmanford(masuk.nextInt(), nodes, numEdges, graph);
+    }
+ 
+    public static edge[] getEdges(int numEdges, int[][] graph) {
+        edge[] rv = new edge[numEdges];
+ 
+        int idx = 0;
+        for (int x = 1; x < graph.length && idx < rv.length; x++) {
+            for (int y = 1; y < graph[x].length && idx < rv.length; y++) {
+                rv[idx] = new edge(x, y);
+                idx = graph[x][y] != 0 ? idx + 1 : idx;
+            }
+        }
+        return rv;
+    }
+ 
+    public static void bellmanford(int src, int nodes, int edges, int[][] graph) {
+        //ngebikin hashmapnya
+        HashMap<Integer, Node> nodesMap = new HashMap<>();
+        
+        for (int i = 1; i < graph.length; i++) {
+            nodesMap.put(i, new Node(i, i == src ? 0 : (int) 1e9 + 1, i == src ? Integer.toString(i) : ""));
+        }
+     
 }
